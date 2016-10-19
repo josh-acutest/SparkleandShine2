ce27a29c35c6e36c2951aa4de8a65ae42446ed78
josh-acutest <josh.crossley@acutesales.co.uk>
2016-10-19 18:05:32 +0100

    jc

---
+++ SparkleandShine2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SparkleandShine2.Global" Language="C#" %>
---
+++ SparkleandShine2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SparkleandShine2.Startup))]
+namespace SparkleandShine2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
